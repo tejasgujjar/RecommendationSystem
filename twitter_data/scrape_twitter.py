06a0a4c4e1c8eb4103b979bfe326650339670db8
yashasm <yashasm1991@gmail.com>
2017-05-07 04:03:09 -0700

    update rating based on twitter feed

--- twitter_data/scrape_twitter.py
+++ twitter_data/scrape_twitter.py
@@ -6,6 +6,29 @@ from tweepy import OAuthHandler
 from textblob import TextBlob
 import sys
 import json
+from __future__ import print_function
+from pymongo import MongoClient
+
+
+def twitter_update(restaurant,state):
+    if state == 0:
+        return True
+    restaurant_name  = restaurant[1:].replace("_"," ").lower()
+    connection = MongoClient("ds117311.mlab.com", 17311)
+    db = connection['restreco']
+    db.authenticate("restUser", "restUser123#")
+    val = 0
+    if state == 1:
+        val = 0.1
+    elif state == -1:
+        val = -0.1
+    db.restaurants_dump.update_one({"name": re.compile(restaurant_name, re.IGNORECASE)}, {
+        '$inc': {
+            'rating': val
+        }
+    }, upsert=False)
+
+    return True
 
 # list of restaurant from the db
 LOOKUP_KEYWORDS = ['pistahouse', 'inchinbamboo'] # to be taken from the mongo DB
